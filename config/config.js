0220fc5252bcb904742095c4337a05b374a18041
Prashant Koshta <prashantkoshta@gmail.com>
2015-10-27 20:37:33 -0500

    Update twitter config.

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
 //config.js
 // Include all configuration variable
+
 module.exports = {
-    url :  process.env.dburl,
+    url : process.env.dburl,
     sessionSecret: process.env.sessionsecret,
     'facebookAuth' : {
-        'clientID'      : process.env.fb_clientid,//'your-secret-clientID-here', // your App ID
-        'clientSecret'  : process.env.fb_clientsecret,//your-client-secret-here', // your App Secret
-        'callbackURL'   : process.env.ssocallbackurl+'/auth/facebook/callback'
+        'clientID'      : process.env.fb_clientid,
+        'clientSecret'  : process.env.fb_clientsecret,
+        'callbackURL'   : process.env.ssocallbackurl + '/auth/facebook/callback'
     },
-
+    
     'twitterAuth' : {
-        'consumerKey'       : process.env.consumerkey,//'your-consumer-key-here',
-        'consumerSecret'    : process.env.consumersecret,//'your-client-secret-here',
-        'callbackURL'       : process.env.ssocallbackurl+'/auth/twitter/callback'//http://localhost:8080/auth/twitter/callback'
+        'consumerKey'       : process.env.tw_consumerkey,
+        'consumerSecret'    : process.env.tw_consumersecret,
+        'callbackURL'       : process.env.ssocallbackurl + '/auth/twitter/callback'
     },
-
+    
     'googleAuth' : {
         'clientID'      : process.env.gg_clientid,
         'clientSecret'  : process.env.gg_clientsecret,
-        'callbackURL'   : process.env.ssocallbackurl+'/auth/google/callback'
+        'callbackURL'   : process.env.ssocallbackurl + '/auth/google/callback'
     }
-}
+};
