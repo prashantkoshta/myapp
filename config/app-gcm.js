53008c4289c001145f0538e0c4bd570c0082241b
Prashant Koshta <prashantkoshta@users.noreply.github.com>
2015-12-01 11:40:25 -0600

    Added

---
+++ config/app-gcm.js
@@ -0,0 +1,32 @@
+// To push notificaiton
+'use strict';
+var config 	= require('./config');
+var gcm 		= require("node-gcm")
+
+var AppGCM  = function(){
+  
+};
+
+AppGCM.prototype.pushNotification = function(req, res, next) {
+
+  //Add your mobile device registration tokens here
+  var regTokens = [config.serverAPIKey-gcm];
+  //Replace your developer API key with GCM enabled here
+  var sender = new gcm.Sender(config.senderId-gcm);
+  var message = new gcm.Message(); 
+
+  message.addData('hello', 'world');
+  message.addNotification('title', 'Hello');
+  message.addNotification('icon', 'ic_launcher');
+  message.addNotification('body', 'World');
+  
+  sender.send(message, regTokens, function (err, response) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(response);
+    }
+  });
+  
+}
+module.exports = new AppGCM();
