05ba5b62cf3a0f6291a2399e07397518a5083d5f
Prashant Koshta <prashantkoshta@users.noreply.github.com>
2015-12-02 12:56:38 -0600

    Added

--- private-static/js/factory/faye-batch.js
+++ private-static/js/factory/faye-batch.js
@@ -1,4 +1,4 @@
 'use strict';
-app.factory('Faye', ["$faye",function($faye) {
+app.factory('svcFaye', ["$faye",function($faye) {
   return $faye("http://peaceful-basin-7772.herokuapp.com/faye");
 }]);
