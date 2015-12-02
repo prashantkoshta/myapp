85ad53e6537c7bb9665940a5c907a5df316200cf
Prashant Koshta <prashantkoshta@users.noreply.github.com>
2015-12-02 15:49:13 -0600

    Added

---
+++ config/faye-conf.js
@@ -0,0 +1,37 @@
+var faye = require('faye')
+var fayeBayeux = function fayeBayeux(){
+    //defining a var instead of this (works for variable & function) will create a private definition
+    var bayeux = new faye.NodeAdapter({
+			    mount: '/faye',
+			    timeout: 45
+	});
+	
+    this.getBayeux = function(){
+        return bayeux;
+    };
+	
+	this.pulishMessage = function(channel,data){
+		bayeux.getClient().publish('/channel-1',data);
+	    console.log('broadcast message:' + req.body.message);
+	    res.send(200);
+	};
+	
+    if(fayeBayeux.caller != fayeBayeux.getInstance){
+        throw new Error("This object cannot be instanciated");
+    }
+}
+/* ************************************************************************
+fayeBayeux CLASS DEFINITION
+************************************************************************ */
+fayeBayeux.instance = null;
+/**
+ * fayeBayeux getInstance definition
+ * @return fayeBayeux class
+ */
+fayeBayeux.getInstance = function(){
+    if(this.instance === null){
+        this.instance = new fayeBayeux();
+    }
+    return this.instance;
+}
+module.exports = fayeBayeux.getInstance();
