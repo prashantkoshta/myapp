374f808b4ad80aa4056e52070f8ec1e516cd3627
Prashant Koshta <prashantkoshta@gmail.com>
2015-12-01 23:36:19 -0600

    Added

--- buildbatch.sh
+++ buildbatch.sh
@@ -6,9 +6,9 @@ mkdir devtool
 cd devtool
 wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 tar -xvzf android-sdk_r24.4.1-linux.tgz
-export PATH=$PATH:/app/devtool/android-sdk-linux/tools
-export PATH=$PATH:/app/devtool/android-sdk-linux/platforms
-export ANDROID_SDK_HOME =$PATH:/app/devtool/android-sdk-linux
+export PATH=$PATH:/tmp/devtool/android-sdk-linux/tools
+export PATH=$PATH:/tmp/devtool/android-sdk-linux/platforms
+export ANDROID_SDK_HOME=$PATH:/tmp/devtool/android-sdk-linux
 cd /tmp
 mkdir dev_tmp
 cd dev_tmp
