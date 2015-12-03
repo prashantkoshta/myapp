d9153374c7ae62978fa46a3cd3f0f392f46ea59f
Prashant Koshta <prashantkoshta@users.noreply.github.com>
2015-12-03 13:33:18 -0600

    Added

--- config/build-project.js
+++ config/build-project.js
@@ -15,27 +15,27 @@ BuildProject.prototype.buildNow = function(callback) {
 	ls = spawn('bash', ['buildbatch.sh']);
 	ls.stdout.on('data', function (data) {
 		//console.log('stdout: ' + data);
-		fayeConf.pulishMessage('/channel-1', { msg: data.toString('utf8')});
+		fayeConf.pulishMessage('/channel-1', { msg: {"mode":"stdout", "error":false,"data":data.toString('utf8')});
 		callback({"mode":"stdout", "error":false,"data":data.toString('utf8')});
 	});
 
 	ls.stderr.on('data', function (data) {
 		//console.log('stderr: ' + data);
-	        fayeConf.pulishMessage('/channel-1', { msg: data.toString('utf8')});
+	        fayeConf.pulishMessage('/channel-1', { msg: {"mode":"stderr", "error":true,"data":data.toString('utf8')});
 		callback({"mode":"stderr", "error":true,"data":data.toString('utf8')});
 	});
 	
 	ls.on('close', function (code) {
 		//console.log('close: ' + code);
 		//console.log("C "+ls.id);
-	        fayeConf.pulishMessage('/channel-1', { msg: data.toString('utf8')});
+	        fayeConf.pulishMessage('/channel-1', { msg: {"mode":"close", "error":false,"data":code}});
 		callback({"mode":"close", "error":false,"data":code});
 	})
 
 	ls.on('exit', function (code) {
 		//console.log('exit: ' + code);
 		//console.log("e "+ls.id);
-		fayeConf.pulishMessage('/channel-1', { msg: data.toString('utf8')});
+		fayeConf.pulishMessage('/channel-1', { msg: {"mode":"exit", "error":false,"data":code}});
 		callback({"mode":"exit", "error":false,"data":code});
 	});
 }
